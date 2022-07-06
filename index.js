b47e7c409f8111b012c5653efef86788d1a0d5f0
surya7826 <suryanarayana7826@gmail.com>
2022-07-06 22:52:31 +0530

    first10

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const path = require('path')
 const PageRoute = require('./routes/PageRoutes')
 const app = express()
 
+const port = process.env.PORT || 5000
+
+
 app.use(express.static('./public'))
 
 
 app.use('',PageRoute)
 
-app.listen(9090,(req,res)=>{
+app.listen(port,(req,res)=>{
     console.log('Server Up And Running');
 })
