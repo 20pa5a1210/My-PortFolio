7073b99a045e60534e8229b8db7d51e37c49fd4c
surya7826 <suryanarayana7826@gmail.com>
2022-07-06 22:38:03 +0530

    first9

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
 const express = require('express')
 const path = require('path')
-// const PageRoute = require('./routes/PageRoutes')
+const PageRoute = require('./routes/PageRoutes')
 const app = express()
 
 app.use(express.static('./public'))
 
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./view/index.html'))
-})
-
-app.get('/home',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./view/index.html'))
-})
-
-app.get('/education',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./view/education.html'))
-})
-
-app.get('/projects',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./view/Projects.html'))
-})
-
-// app.use('',PageRoute)
+app.use('',PageRoute)
 
 app.listen(9090,(req,res)=>{
     console.log('Server Up And Running');
